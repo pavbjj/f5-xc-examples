28d95a6c9c163c7308021142d437e3139d61d15e
pavbjj <43717447+pavbjj@users.noreply.github.com>
2023-09-21 10:46:45 +0100

    Update main.tf

--- AWS-backend/main.tf
+++ AWS-backend/main.tf
@@ -6,9 +6,9 @@ resource "volterra_aws_vpc_site" "aws_site" {
   default_blocked_services = true
 
   aws_cred {
-    name      = "p-kuligowski-aws-cc-manual"
+    name      = "my-creds"
     namespace = "system"
-    tenant    = "f5-consult-prqnkato"
+    tenant    = "mytenant-prqnkato"
   }
 
   vpc {
@@ -40,7 +40,6 @@ resource "volterra_aws_vpc_site" "aws_site" {
     performance_enhancement_mode {
       perf_mode_l7_enhanced = true
     }
-
   }
 
   custom_security_group {
@@ -48,7 +47,6 @@ resource "volterra_aws_vpc_site" "aws_site" {
     inside_security_group_id  = "sg-2"
   }
 
-  # total_nodes = "1"
   direct_connect_disabled = true
   instance_type           = "t3.xlarge"
   disable_internet_vip    = true
@@ -56,10 +54,3 @@ resource "volterra_aws_vpc_site" "aws_site" {
   ssh_key       = "ssh-rsa "
 }
 
-# resource "volterra_tf_params_action" "apply_aws_vpc" {
-#   site_name        = volterra_aws_vpc_site.aws_site.name
-#   site_kind        = "aws_vpc_site"
-#   action           = "apply"
-#   wait_for_action  = true
-#   ignore_on_update = true
-# }
